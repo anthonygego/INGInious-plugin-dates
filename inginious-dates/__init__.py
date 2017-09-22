ce6db3d08c863accd5427c595e5c2451eeab1d6e
Anthony Gégo <anthonygego@hotmail.com>
2017-09-22 11:32:15 +0200

    Commit code

---
+++ inginious-dates/__init__.py
@@ -0,0 +1,18 @@
+import re
+from inginious.frontend.webapp.accessible_time import AccessibleTime
+
+
+def task_accessibility(course, task, default):
+    dates = course.get_descriptor().get("dates", None)
+    if not dates:
+        return default
+
+    for pattern in dates.keys():
+        if re.match(pattern, task.get_id()):
+            return AccessibleTime(dates[pattern])
+
+    return default
+
+
+def init(plugin_manager, course_factory, client, config):
+    plugin_manager.add_hook('task_accessibility', task_accessibility)
